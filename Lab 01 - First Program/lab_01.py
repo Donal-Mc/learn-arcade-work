51551d1e01de8bdb44828d74fc10b8939d7e821c
Donal Mackin <donal.mackin@my.simpson.edu>
2021-09-09 12:13:49 -0500

    Work on Lab 1

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,6 @@
-
+print("This is my first time coding and it's fun.")
+print("This is a basic piece of code designed to fulfill the requirements for our first lab.")
+print("Upon completion this code should consist of \"4 separate print commands with at least one escape code.\". ")
+print("""Dr Carver is
+the best computer science teacher
+in the Midwest.""")
---
+++ Lab 02 - Draw a Picture/lab_02.py
@@ -0,0 +1,6 @@
+"""
+This is a sample program to show how to draw using Python and the Arcade library.
+"""
+import arcade
+arcade.open_window(600, 600, "drawing example")
+arcade.run()
