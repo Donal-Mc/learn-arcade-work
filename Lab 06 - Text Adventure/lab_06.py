2111a4a2b6fcf7642e497728d11f593223630e1d
donal-mc <donal.mackin@my.simpson.edu>
2021-10-13 22:24:34 -0500

    Lab 06

--- Lab 06 - Text Adventure/lab_06.py
+++ Lab 06 - Text Adventure/lab_06.py
@@ -1,1 +1,8 @@
-Placeholder
+class Room:
+    """
+    This is a class that represents the Room
+    """
+    def __init__(self):
+        """
+        This is a method that sets up the variables in the Room
+        """
